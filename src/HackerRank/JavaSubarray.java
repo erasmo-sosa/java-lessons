65c1dac85069730914cf18ae210bc5e58a61768f
Erasmo Sosa <erasmo.sosa@outlook.com>
2023-04-21 10:24:32 -0400

    initial commit

--- src/HackerRank/JavaSubarray.java
+++ src/HackerRank/JavaSubarray.java
@@ -8,22 +8,24 @@ public class JavaSubarray {
 		
 		int negativeArrCount = 0;
 		
-	    int arrLenght = 5 -1;
+	    //int arrLenght = 5;
 	    String spaceSepInt = "1 -2 4 -5 1";
 	    String strArray[] = spaceSepInt.split(" ");
-	    int intArray[] = new int[arrLenght];
+	    System.out.println("strArray.length: " + strArray.length);
 	    
-	    for(int i=0; i < arrLenght; i++) {
+	    int intArray[] = new int[strArray.length];
+	    
+	    for(int i=0; i < intArray.length; i++) {
 	    	intArray[i] = Integer.parseInt(strArray[i]);
 	    }
 	    
-	    for(int i=0; i < arrLenght; i++) {
-	    	for(int j=i; i < arrLenght; j++) {
+	    for(int i=0; i < intArray.length; i++) {
+	    	for(int j=i; i < strArray.length; j++) {
 	    		int current_sum = 0;
 	    		for(int k=i; k < j; k++) {
 	    			System.out.println("intArray[k]:" + intArray[k]);
 	    			current_sum =+intArray[k];
-	    			System.out.println("current_sum" + current_sum);
+	    			System.out.println("current_sum: " + current_sum);
 	    		}
 	    	}
 	    }
